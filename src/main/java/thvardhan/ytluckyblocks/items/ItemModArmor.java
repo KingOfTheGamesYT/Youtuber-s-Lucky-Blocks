4292c67966d55c5044f15ec43436e629830a659f
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2023-11-01 20:23:58 -0400

    ported over a bunch of armor
    over a full page in the creative tab

--- src/main/java/thvardhan/ytluckyblocks/items/ItemModArmor.java
+++ src/main/java/thvardhan/ytluckyblocks/items/ItemModArmor.java
@@ -4,13 +4,14 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemGroup;
+import thvardhan.ytluckyblocks.Main;
 
 
 public class ItemModArmor extends ArmorItem {
 
     public ItemModArmor(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builder) {
 
-        super(materialIn, slot, new Properties().group(ItemGroup.COMBAT));
+        super(materialIn, slot, new Properties().group(Main.MAIN));
     }
 
 
--- src/main/java/thvardhan/ytluckyblocks/items/ModArmorMaterial.java
+++ src/main/java/thvardhan/ytluckyblocks/items/ModArmorMaterial.java
@@ -17,9 +17,47 @@ public enum ModArmorMaterial implements IArmorMaterial {
     YOUTUBE(Main.MODID + ":ytarmor", 100, new int[] { 4, 7, 5, 4}, 1,
              SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
 
-    DUCK(Main.MODID + ":ytarmor", 100, new int[] { 4, 7, 5, 4}, 1,
+    DUCK(Main.MODID + ":duckarmor", 80, new int[] { 4, 8, 6, 4}, 1,
     SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
 
+    LEAH(Main.MODID + ":leaharmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    MAX(Main.MODID + ":maxarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    CASSIETHECAT(Main.MODID + ":cassiethecat", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    DONUT(Main.MODID + ":donutarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    EVILLITTLEKELLY(Main.MODID + ":evillittlekellyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    LITTLE_ALLY(Main.MODID + ":littleallyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    LITTLECARLY(Main.MODID + ":littlecarlyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    LITTLE_DONNY(Main.MODID + ":littledonnyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    LITTLEKELLY(Main.MODID + ":littlekellyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    LITTLE_LIZARDGAMING(Main.MODID + ":littlelizardgaming", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    MAX_THEMONKEY(Main.MODID + ":maxthemonkeyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    SHARK_Y(Main.MODID + ":sharkyarmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F),
+
+    TINY_TURTLE(Main.MODID + ":tinyturtulearmor", 80, new int[] { 4, 8, 6, 4}, 1,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F);
     private static final int[] MAX_DAMAGE_ARRAY = new int[] {11, 16, 15, 13};
     private final String name;
     private final int maxDamageFactor;
